f6274905f31d6d42970a354dee6e1e0860011b96
urayoshie <nof.q.p1417@gmail.com>
2021-06-26 14:55:53 +0900

    2021-06-26

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,35 @@
+const path = require('path');
+const outputPath = path.resolve(__dirname, 'dist');
+
 module.exports = {
-  mode: 'development',
+  output: {
+    filename: '[name].js',
+    path: outputPath,
+    clean: true
+  },
   entry: {
     news: ['./js/news.js'],
     works: ['./js/works.js'],
     contact: ['./js/contact.js'],
   },
-  output: {
-    filename: '[name].js',
-  },
+  // エラーを回避するため
+  devtool: 'eval-source-map',
   devServer: {
-    index: 'index.html',
-    open: true,
+    // ディレクトリを指定（必要）
+    contentBase: './',
+    // 開くファイル（index.html を開く）
+    openPage: './',
+  },
+  module: {
+    rules: [
+      {
+        // 対象のファイル（.js, .mjs ので終わるファイル全て）
+        test: /\.m?js$/i,
+        // node_modules ディレクトリは対象外とする
+        exclude: /node_modules/,
+        // 使用するローダー
+        use: 'babel-loader',
+      },
+    ],
   },
 };
